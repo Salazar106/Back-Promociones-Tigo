a737d12fca6fbd15457989e311023b1853aa941d
Salazar106 <jdps106@hotmail.com>
2023-01-30 07:29:25 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+const express = require("express"); // import express
+const app = express(); // create express app
+const cors = require("cors");
+
+app.set("port", process.env.PORT || 3200); // set the port
+const whiteList = ["http://localhost:3000"];
+
+app.use(express.json()); // for parsing application/json
+
+app.use(
+  cors({
+    origin: whiteList,
+  })
+);
+
+app.listen(app.get("port"), () => {
+  console.log("")
+  console.log("")
+  console.log("*****************************************************");
+  console.log("*   🖥️  Servidor establecido en el puerto:", app.get("port") , "🖥️   *");
+}); //poner en el puerto 3000
+
+//!----------- Rutas--------------
+
+
+//?--------Ruta de PromoHogares--------
+
+app.use(require("./routes/promocionesHogar/excelToJson"));
+
+app.use(require("./routes/ejemplo/ejemplo"))
---
+++ src/routes/promocionesHogar/excelToJson.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const mySqlConnection = require("../../conexion");
+
+//? ---------buscamos todos los datos---------
+router.get("/allData", (req, res) => {
+  const query = "SELECT * FROM matrizHogar";
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+//? ---------Convertir excel a Json----------
+
+
+
+
+
+
+
+
+
+  module.exports = router;
