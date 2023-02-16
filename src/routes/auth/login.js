17173474de06cca7c9026c07fee4b3b48ac04217
Daniel Salazar <jdps106@hotmail.com>
2023-02-16 15:13:37 -0500

    arreglo validacion de login

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -37,25 +37,62 @@ router.post("/login", async (req, res) => {
   const loginQuery =
     "select u.*, r.nombre as rol,GROUP_CONCAT(a.id_unidad) as accesos, u2.nombre  FROM usuario u join rol r on u.id_rol = r.id join acceso a on r.id = a.id_rol JOIN unidad u2 on u2.id = a.id_unidad where u.documento = ?";
 
-  mySqlConnection.query(validateQuery, [validateQuery], (err, row, fields) => {
-    if (row==="") {
-      mySqlConnection.query(loginQuery, [documento], (err, rows, fields) => {
-        if (!err) {
-          if (rows.length >= 1) {
-            compare(documento, password, rows);
-          }
-        } else {
+  mySqlConnection.query(loginQuery, [documento], async (err, rows, fields) => {
+    if (!err) {
+      if (rows.length >= 1) {
+        if (rows[0].estado === 0) {
           res.json({
-            status: "no estas registrado",
+            status: "usuario inactivo",
             statusCode: 403,
           });
         }
-      });
-    } else {
-      res.json({
-        satus: 404,
-        msg: "Usuario no registrado",
-      });
+        compare(documento, password, rows);
+      } else {
+        res.json({
+          status: "no estas registrado",
+          statusCode: 403,
+        });
+      }
+    }
+  });
+});
+
+router.post("/login2", (req, res) => {
+  const { documento, password } = req.body;
+  const validateQuery = "SELECT * FROM usuario WHERE documento = ?";
+  const loginQuery =
+    "select u.*, r.nombre as rol,GROUP_CONCAT(a.id_unidad) as accesos, u2.nombre  FROM usuario u join rol r on u.id_rol = r.id join acceso a on r.id = a.id_rol JOIN unidad u2 on u2.id = a.id_unidad where u.documento = ?";
+
+  mySqlConnection.query(validateQuery, [documento], (err, usuario, fields) => {
+    if (!err) {
+      if (usuario.length != 0) {
+        mySqlConnection.query(loginQuery, [documento], (err, row, fields) => {
+          let compare = bcryptjs.compareSync(password, row[0].password);
+          if (compare) {
+            jwt.sign(
+              {
+                documento: documento,
+                password: row[0].password,
+              },
+              "secretkey",
+              (err, token) => {
+                res.status(200).json({
+                  status: "Login exitoso",
+                  accesos: row[0].accesos,
+                  estado: row[0].estado,
+                  token: token,
+                });
+              }
+            );
+          } else {
+            res.status(403).json({
+              status: "contraseña incorrecta",
+            });
+          }
+        });
+      } else {
+        res.status(400).json({ msg: "No Te encontre" });
+      }
     }
   });
 });
