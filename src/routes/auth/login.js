24e686b437f130a4a89d526a75ef79f315c4c6a2
Daniel Salazar <jdps106@hotmail.com>
2023-02-16 15:25:43 -0500

    se valida estado del usuario para poder logearse

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -4,60 +4,7 @@ const mySqlConnection = require("../../conexion");
 const bcryptjs = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-router.post("/login", async (req, res) => {
-  function compare(documento, password, usuarioRegistrado) {
-    let compare = bcryptjs.compareSync(password, usuarioRegistrado[0].password);
-    if (compare) {
-      jwt.sign(
-        {
-          documento: documento,
-          password: usuarioRegistrado[0].password,
-        },
-        "secretkey",
-        (err, token) => {
-          res.json({
-            status: "Login exitoso",
-            statusCode: 200,
-            accesos: usuarioRegistrado[0].accesos,
-            estado: usuarioRegistrado[0].estado,
-            token: token,
-          });
-        }
-      );
-    } else {
-      res.json({
-        status: "contraseña incorrecta",
-        statusCode: 403,
-      });
-    }
-  }
-
-  const { documento, password } = req.body;
-  const validateQuery = "SELECT * FROM usuario WHERE documento = ?";
-  const loginQuery =
-    "select u.*, r.nombre as rol,GROUP_CONCAT(a.id_unidad) as accesos, u2.nombre  FROM usuario u join rol r on u.id_rol = r.id join acceso a on r.id = a.id_rol JOIN unidad u2 on u2.id = a.id_unidad where u.documento = ?";
-
-  mySqlConnection.query(loginQuery, [documento], async (err, rows, fields) => {
-    if (!err) {
-      if (rows.length >= 1) {
-        if (rows[0].estado === 0) {
-          res.json({
-            status: "usuario inactivo",
-            statusCode: 403,
-          });
-        }
-        compare(documento, password, rows);
-      } else {
-        res.json({
-          status: "no estas registrado",
-          statusCode: 403,
-        });
-      }
-    }
-  });
-});
-
-router.post("/login2", (req, res) => {
+router.post("/login", (req, res) => {
   const { documento, password } = req.body;
   const validateQuery = "SELECT * FROM usuario WHERE documento = ?";
   const loginQuery =
@@ -65,7 +12,7 @@ router.post("/login2", (req, res) => {
 
   mySqlConnection.query(validateQuery, [documento], (err, usuario, fields) => {
     if (!err) {
-      if (usuario.length != 0) {
+      if (usuario.length != 0 && usuario[0].estado==1) {
         mySqlConnection.query(loginQuery, [documento], (err, row, fields) => {
           let compare = bcryptjs.compareSync(password, row[0].password);
           if (compare) {
@@ -91,7 +38,7 @@ router.post("/login2", (req, res) => {
           }
         });
       } else {
-        res.status(400).json({ msg: "No Te encontre" });
+        res.status(400).json({ msg: "Ocurrio un error, vuelve a intentar" });
       }
     }
   });
